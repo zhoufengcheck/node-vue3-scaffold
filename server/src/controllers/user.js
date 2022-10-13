3f2e06ae5abe0430d1db28dafe5668fd6f226c40
zhoufengcheck <119749116@qq.com>
2022-10-13 19:57:52 +0800

    change

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -3,6 +3,11 @@ import normalLog from '../utils/normalLog.js'
 import {setRedis, getRedis} from '../utils/redis.js';
 import { uid } from 'uid'
 import schedule from 'node-schedule';
+import fs from 'fs-extra';
+import { fileURLToPath } from 'url';
+import path from 'path'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 class User {
     constructor(){
         normalLog({nam:1})
@@ -27,36 +32,77 @@ class User {
             }
         });
     }
+    // async getAllUserList(req, res){
+    //     let list = await UserService.findData({
+    //         $where: function(){
+    //             return this.user_name == 'zhou1feng'
+    //         }
+    //     })
+    //     await new Promise((res,rej)=>{
+    //         setTimeout(()=>{
+    //             res(1)
+    //         },3000)
+    //     })
+    //     // res.set('Cache-control', 'max-age=10000')
+    //     res.send({
+    //         status:true,
+    //         data:list,
+    //         a:1
+    //     })
+    // }
     async getAllUserList(req, res){
-        console.log(req.data)
-        let list = await UserService.findData({
-            $where: function(){
-                return this.user_name == 'zhou1feng'
-            }
-        })
-        await new Promise((res,rej)=>{
-            setTimeout(()=>{
-                res(1)
-            },3000)
-        })
-        // res.set('Cache-control', 'max-age=10000')
-        res.send({
-            status:true,
-            data:list,
-            a:1
-        })
+        //设置静态资源
+        let url = path.join(__dirname,'../../node_modules.zip')
+        let size = fs.statSync(url).size || null
+        // let data = fs.createReadStream(path.join(__dirname,'../../node_modules.zip'))
+        let range = req.headers["range"];
+        console.log(range)
+         //获取分片的开始和结束位置
+        let bytesV = range.split("=");
+        bytesV.shift()
+        let [start, end] = bytesV.join('').split("-");
+        start = Number(start)
+        end = Number(end)
+        //分片开始 结束位置不对 拒绝下载
+        if (start > size || end > size) {
+          res.set({ "Content-Range": `bytes */${size}`});
+          res.status(416).send(null);
+          return;
+        }
+        //开始分片下载
+        res.status(206);
+        res.set({
+            "Accept-Ranges": "bytes",
+            "Content-Range": `bytes ${start}-${end ? end : size}/${size}`,
+        });
+
+        console.log(start + '---' + end)
+        fs.createReadStream(url, { start, end }).pipe(res);
+
+
+        // let list = await UserService.findData({
+        //     $where: function(){
+        //         return this.user_name == 'zhou1feng'
+        //     }
+        // })
+        // res.send({
+        //     status:true,
+        //     // data:list,
+        //     a:1
+        // })
+        // data.pipe(res);
     }
-    async getAllUserList(req, res){
-        let list = await UserService.findData({
-            $where: function(){
-                return this.user_name == 'zhou1feng'
-            }
-        })
-        res.send({
-            status:true,
-            data:list,
-            a:1
-        })
+    async getSize(req,res){
+         //获取要下载文件的路径
+         let filePath = path.join(__dirname,'../../node_modules.zip')
+         //console.log(filePath)
+         //获取文件的大小
+         let size = fs.statSync(filePath).size || null
+         console.log('下载文件大小' + size)
+         res.send({
+           msg:'ok',
+           data:size.toString()
+         })
     }
     async getData(req,res) {
         res.send({
